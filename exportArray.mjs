fb6215ebf6bc4ece7b3284cb89d6a73497b31d3f
SrijaReddy99 <21bd1a0555csec@gmail.com>
2023-05-25 10:58:59 +0000

    import and export of array

---
+++ exportArray.mjs
@@ -0,0 +1,2 @@
+const array_sample = ["countries", 190, "continents", 7, false, 6.2];
+export default array_sample;
---
+++ importArray.mjs
@@ -0,0 +1,2 @@
+import array_sample from "./exportArray.mjs";
+console.log(array_sample);
